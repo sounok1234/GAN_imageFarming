ecd9795021ee13bba39245e025c69d3ce0cb09b0
Dan Siroky <dan.siroky@hok.com>
2020-02-04 16:21:23 -0600

    Create functions to start/stop/resume/clear timer

--- timer.js
+++ timer.js
@@ -1,4 +1,5 @@
-//window.onload = timer;
+let timerInterval = null;
+let timerSeconds = 0;
 /*
 var timeList = [2, 4, 6, 8];
 var int = Math.floor(Math.random() * 4);
@@ -7,16 +8,40 @@ function pad(d) {
     return (d < 10) ? '0' + d.toString() : d.toString();
 }
 
-function timer(){
-    //console.log(timeList[int])
-    var sec = 0;
-    var timer = setInterval(function(){
-        document.getElementById("safeTimerDisplay").style.fontFamily = "Impact,Charcoal,sans-serif";
-        document.getElementById("safeTimerDisplay").style.fontSize = "xx-large";
-        document.getElementById('safeTimerDisplay').innerHTML= Math.floor(sec/60).toString() + ':' + pad((sec%60)).toString();
-        sec++;
-        if (sec < 0) {
-            window.clearTimeout(timer);
-        }
-    }, 1000);
+function setTime(timeInSec) {
+    document.getElementById('timerDisplay').innerHTML= Math.floor(timeInSec/60).toString() + ':' + pad((timeInSec%60)).toString();
+}
+
+function startTimer(initialValue){
+    console.log(timerInterval);
+    if (timerInterval == null) {
+        timerSeconds =  initialValue || 0;
+        timerInterval = setInterval(function(){
+            const timerDisplay = document.getElementById('timerDisplay');
+            timerDisplay.style.fontFamily = "monospace";
+            timerDisplay.style.fontSize = "2rem";
+            setTime(timerSeconds);
+            timerSeconds++;
+            if (timerSeconds < 0) {
+                window.clearTimeout(timerInterval);
+            }
+        }, 1000);
+    }
+}
+
+function clearTimer() {
+    window.clearInterval(timerInterval);
+    timerInterval = null;
+    timerSeconds = 0;
+    setTime(0);
+}
+
+function stopTimer() {
+    window.clearInterval(timerInterval);
+    timerInterval = null;
+    setTime(timerSeconds);
+}
+
+function resumeTimer() {
+    startTimer(timerSeconds);
 }
